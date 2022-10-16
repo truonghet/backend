41330d6a9f67cf1d03bb766c96bca3264590cadb
Het Truong <truongvanhetln@gmail.com>
2022-10-16 11:40:26 +0700

    update

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo 'run after_install.sh: ' >> /home/ec2-user/backend/deploy.log
 
-echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/backend/deploy.log
+echo 'cd /home/ec2-user/backend' >> /home/ec2-user/backend/deploy.log
 cd /home/ec2-user/backend >> /home/ec2-user/backend/deploy.log
 
 echo 'npm install' >> /home/ec2-user/backend/deploy.log 
