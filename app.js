f0dcd437d807c160290d22e19f25f0281b176c55
Het Truong <truongvanhetln@gmail.com>
2022-10-16 19:00:34 +0700

    Increase version

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const hostname = '127.0.0.1'; // Your server ip address
 const port = 3000;
 
-const version = 'v0.4';
+const version = 'v0.5';
 
 app.get('/', (req, res) => {
     // set response content    
