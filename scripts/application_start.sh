0102eef9b3015cec195d826ee033867b312f906a
Het Truong <truongvanhetln@gmail.com>
2022-10-16 18:32:33 +0700

    Update pm2 command

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,4 +4,4 @@
 echo 'run application_start.sh: ' >> /home/ec2-user/backend/deploy.log
 
 echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/backend/deploy.log
-pm2 restart nodejs-express-app >> /home/ec2-user/backend/deploy.log
+pm2 start app.js --name "app-name" >> /home/ec2-user/backend/deploy.log
