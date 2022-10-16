2adc4a5e141145e17991dc08d5fcb3a671fd73c5
Het Truong <truongvanhetln@gmail.com>
2022-10-16 18:39:08 +0700

    update codes

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,5 +3,5 @@
 
 echo 'run application_start.sh: ' >> /home/ec2-user/backend/deploy.log
 
-echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/backend/deploy.log
-pm2 start app.js --name "app-name" >> /home/ec2-user/backend/deploy.log
+echo 'pm2 start app.js' >> /home/ec2-user/backend/deploy.log
+pm2 start app.js >> /home/ec2-user/backend/deploy.log
