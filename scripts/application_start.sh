64cf556e661fb49609c6c56b56e714646453c7c3
Het Truong <truongvanhetln@gmail.com>
2022-10-16 18:43:28 +0700

    Update app name

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,5 +3,5 @@
 
 echo 'run application_start.sh: ' >> /home/ec2-user/backend/deploy.log
 
-echo 'pm2 start app.js' >> /home/ec2-user/backend/deploy.log
-pm2 start app.js >> /home/ec2-user/backend/deploy.log
+echo 'pm2 restart mynodeapp' >> /home/ec2-user/backend/deploy.log
+pm2 restart mynodeapp >> /home/ec2-user/backend/deploy.log
