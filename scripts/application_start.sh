f2d9b894fd5dbc4b6ba98c395e7c347772c8750a
Het Truong <truongvanhetln@gmail.com>
2022-10-16 18:50:02 +0700

    update command

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,4 +4,4 @@
 echo 'run application_start.sh: ' >> /home/ec2-user/backend/deploy.log
 
 echo 'pm2 restart mynodeapp' >> /home/ec2-user/backend/deploy.log
-pm2 restart mynodeapp >> /home/ec2-user/backend/deploy.log
+sudo pm2 restart mynodeapp >> /home/ec2-user/backend/deploy.log
