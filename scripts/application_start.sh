7f4f6d5f3203b84f9cae9ab8d73433320d0f5698
Het Truong <truongvanhetln@gmail.com>
2022-10-16 18:24:47 +0700

    Update directory

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,5 +3,5 @@
 
 echo 'run application_start.sh: ' >> /home/ec2-user/backend/deploy.log
 
-echo 'pm2 restart myapp' >> /home/backend/deploy.log
+echo 'pm2 restart myapp' >> /home/ec2-user/backend/deploy.log
 pm2 restart myapp >> /home/ec2-user/backend/deploy.log
