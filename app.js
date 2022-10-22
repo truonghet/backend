d5c97f0cabb99708af3005cbdfa641257b36edaa
Het Truong <truongvanhetln@gmail.com>
2022-10-22 21:30:32 +0700

    123

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const app = express();
-const hostname = '127.0.0.1'; // Your server ip address
+const hostname = '127.0.0.1';
 const port = 3000;
 
-const version = 'v0.2';
+const version = 'v0.1';
 
 app.get('/', (req, res) => {
-    // set response content    
-        res.send(`<html>
-                    <body>
-                        <h1 style="color:blue;text-align: center;margin-top: 100px;"> [Version ${version}]: CI/CD - Codedeploy - GithubAction-HetTV</h1>
+    res.send(`<html>
+                    <head>
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    </head>
+                    <body style="background-color:#232f3e">
+                        <h1 style="color:#EC7211 !important;text-align: center;margin-top: 0;"> [Version ${version}]: CI/CD - Codedeploy - GithubAction-HetTV</h1>
                         <div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%)">
-                            <img src="https://picsum.photos/400/400?random=1">
+                            <img src="https://picsum.photos/1500/600?random=1">
                         </div>
                     </body>
                    </html>`);
- 
-  console.log(`[Version ${version}]: New request => http://${hostname}:${port}`+req.url);
+
+    console.log(`[Version ${version}]: New request => http://${hostname}:${port}` + req.url);
 
 })
 
